d9775842da90d798739614d91dd387aff5bf667c
Jongmin Kim <jmkim@pukyong.ac.kr>
2017-05-11 11:18:29 +0900

    Add comment to Yahoo Finance example

--- examples/yahoo_finance.cpp
+++ examples/yahoo_finance.cpp
@@ -1,5 +1,9 @@
 #include "../src/RetrieveData.hpp"
 
+/*
+    Retrieve the NASDAQ Composite (^IXIC) stock data from Yahoo Finance
+    between 2014-05-08 and 2017-05-05 with interval 1 day.
+*/
 int main ()
 {
   stock_crawler::RetrieveDataFromYahooFinance ("^IXIC", "1399474800", "1494169200", "1d");
